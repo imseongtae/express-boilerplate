572638f26b84530b8575a4e64a666563d580ffd6
imseongtae <dev.haemil@gmail.com>
2020-12-09 03:13:28 +0900

    chore: Add project config

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	port: 3000,
+	production: {
+		// host: 'http://127.0.0.1',
+		host: 'http://localhost',
+	},
+	development: {
+		// http://localhost:4000/
+		host: 'http://localhost',
+	},
+	test: {
+		host: 'http://localhost',
+	},
+};
