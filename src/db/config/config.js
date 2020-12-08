e92341ce4c64d491e8958b878c3cdc048507cfc0
imseongtae <dev.haemil@gmail.com>
2020-12-09 03:12:49 +0900

    chore: Update db config

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,28 +1,40 @@
 // const path = require('path');
 // const storage = path.join(__dirname, '../../../db.sqlite');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
 	development: {
 		username: 'root',
 		password: '12345',
-		database: 'express',
+		database: 'backend',
 		host: '127.0.0.1',
 		dialect: 'mysql',
+		timezone: '+09:00',
+		dialectOptions: {
+			charset: 'utf8mb4',
+			dateStrings: true,
+			typeCast: true,
+		},
+		logging: false,
 	},
 	test: {
-		username: 'root',
-		password: '12345',
-		database: 'express_test',
-		host: '127.0.0.1',
-		dialect: 'mysql',
+		dialect: 'sqlite',
+		storage: ':memory',
 		logging: false,
 	},
 	production: {
-		username: 'root',
-		password: null,
-		database: 'database_production',
-		host: '127.0.0.1',
+		username: process.env.DB_ADMIN,
+		password: process.env.DB_PASSWORD,
+		database: 'backend',
+		host: process.env.DB_HOST,
 		dialect: 'mysql',
+		timezone: '+09:00',
+		dialectOptions: {
+			charset: 'utf8mb4',
+			dateStrings: true,
+			typeCast: true,
+		},
 		logging: false,
 	},
 };
